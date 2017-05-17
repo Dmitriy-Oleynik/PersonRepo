27c444aa72280e39023584e3b747b4616025d106
Dmitriy Oleynik <081276odl@gmail.com>
2017-05-17 08:32:15 +0300

    GIT START 2 commit

---
+++ GIT_Start_Person/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_Start_Person
+{
+    class Person
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+    }
+}
