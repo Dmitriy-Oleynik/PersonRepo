88cd7c2d29d213fd32b3373f83f9132409addd12
Dmitriy Oleynik <081276odl@gmail.com>
2017-05-18 08:37:27 +0300

    18.05.2017 commit 2

---
+++ GIT_Start_Person/Worker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_Start_Person
+{
+    class Worker: Person
+    {
+        public string Company { set; get; }
+    }
+}
